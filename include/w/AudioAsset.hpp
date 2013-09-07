8630f7d684122ae97ce6b7691694d22c6a89a840
Antti Peuhkurinen <antti.peuhkurinen@woimasolutions.com>
2013-09-07 21:00:47 +0300

    Initial audio API

---
+++ include/w/AudioAsset.hpp
@@ -0,0 +1,83 @@
+/**
+ * libw
+ *
+ * Copyright (C) 2013 Woima Solutions
+ *
+ * This software is provided 'as-is', without any express or implied warranty. In
+ * no event will the authors be held liable for any damages arising from the use
+ * of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including
+ * commercial applications, and to alter it and redistribute it freely, subject to
+ * the following restrictions:
+ *
+ *  * The origin of this software must not be misrepresented; you must not claim
+ *    that you wrote the original software. If you use this software in a product,
+ *    an acknowledgment in the product documentation is appreciated.
+ *
+ *  * Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ *  * This notice may not be removed or altered from any source distribution.
+ *
+ * @author antti.peuhkurinen@woimasolutions.com
+ */
+
+#ifndef LIBW_AUDIOASSET
+#define LIBW_AUDIOASSET
+
+#include <w/Class.hpp>
+
+/**
+ * AudioAsset is a class for sound samples.
+ *
+ * Supported format(s):
+ *  - wav, PCM mono 44100Hz 16bit, signed integer, linear, little endian
+ */
+namespace w
+{
+    class AudioAsset
+    {
+    public:
+        COPYABLE(AudioAsset)
+
+        /**
+         * Creates audio asset
+         *
+         * @note Resources are automatically shared meaning that same audio resource
+         * file is loaded only once.
+         *
+         * @param [in]  filename    filename asset
+         */
+        AudioAsset(const char* filename);
+        ~AudioAsset();
+
+        /**
+         * Plays the sample.
+         *
+         * @param [in]  volume      Volume level [0.0 - 1.0], value is checked & limited.
+         *
+         * @note AudioEngine must exist to succesfully make a play() call.
+         */
+        void play(float volume = 1.0f);
+
+        /**
+         * Set volume level.
+         *
+         * @param [in]  volume      Volume level [0.0 - 1.0], value is checked & limited.
+         */
+        void setVolume(float volume);
+
+        /**
+         * Fades the sample away with linear fade.
+         *
+         * @param [in]  fadeOutTime     Millisecond time to fadeout the sound.
+         */
+        void fadeOut(unsigned int fadeOutTimeMilliseconds = 0);
+
+    private:
+        class AudioAssetPrivate* private_;
+    };
+}
+
+#endif
---
+++ include/w/Class.hpp
@@ -0,0 +1,44 @@
+/**
+ * libw
+ *
+ * Copyright (C) 2013 Woima Solutions
+ *
+ * This software is provided 'as-is', without any express or implied warranty. In
+ * no event will the authors be held liable for any damages arising from the use
+ * of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including
+ * commercial applications, and to alter it and redistribute it freely, subject to
+ * the following restrictions:
+ *
+ *  * The origin of this software must not be misrepresented; you must not claim
+ *    that you wrote the original software. If you use this software in a product,
+ *    an acknowledgment in the product documentation is appreciated.
+ *
+ *  * Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ *  * This notice may not be removed or altered from any source distribution.
+ *
+ * @author antti.peuhkurinen@woimasolutions.com
+ */
+
+#ifndef LIBW_CLASS
+#define LIBW_CLASS
+
+namespace w
+{
+    #define COPYABLE(ClassName) \
+        ClassName(ClassName const& instance); \
+        ClassName& operator=(ClassName const& r);
+
+    #define UNCOPYABLE(ClassName) \
+    private: \
+        Uncopyable(const Uncopyable&); \
+        Uncopyable& operator=(const Uncopyable&); \
+    public: \
+        Uncopyable() {}; \
+        virtual ~Uncopyable() {};
+}
+
+#endif
