07a83a0a048cca868c151395428a8f9f36c893a9
Antti Peuhkurinen <antti.peuhkurinen@woimasolutions.com>
2013-09-07 22:27:45 +0300

    Referenced and Resource files

---
+++ src/Referenced.hpp
@@ -0,0 +1,87 @@
+/**
+ * libw
+ *
+ * Copyright (C) 2013 Woima Solutions
+ *
+ * This software is provided 'as-is', without any express or implied warranty. In
+ * no event will the authors be held liable for any damages arising from the use
+ * of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including
+ * commercial applications, and to alter it and redistribute it freely, subject to
+ * the following restrictions:
+ *
+ * 1) The origin of this software must not be misrepresented; you must not claim
+ *    that you wrote the original software. If you use this software in a product,
+ *    an acknowledgment in the product documentation is appreciated.
+ *
+ * 2) Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3) This notice may not be removed or altered from any source distribution.
+ *
+ * @author antti.peuhkurinen@woimasolutions.com
+ */
+
+#ifndef LIBW_REFERENCED
+#define LIBW_REFERENCED
+
+#include <cstddef>
+using std::ptrdiff_t; // NOTE: sigc++ needs this line
+#include <sigc++/signal.h>
+
+namespace w
+{
+    class Referenced
+    {
+    public:
+        sigc::signal<void, int> destroy;
+
+        unsigned int increment()
+        {
+            unsigned int r = ++referenceCount_;
+            return r;
+        }
+
+        unsigned int decrement()
+        {
+            unsigned int r = --referenceCount_;
+            if (r <= 0)
+            {
+                destroy.emit(id_);
+                delete this;
+            }
+            return r;
+        }
+
+        unsigned int referenceCount()
+        {
+            return referenceCount_;
+        }
+
+        unsigned int id()
+        {
+            return id_;
+        }
+
+    protected:
+        Referenced():
+            referenceCount_(0),
+            id_(lastId_++)
+        {
+        }
+
+        virtual ~Referenced()
+        {
+        }
+
+    private:
+        unsigned int referenceCount_;
+        unsigned int id_;
+
+        // TODO: locking for thread safety
+        static unsigned int lastId_;
+    };
+}
+
+#endif
---
+++ src/Resource.hpp
@@ -0,0 +1,46 @@
+/**
+ * libw
+ *
+ * Copyright (C) 2013 Woima Solutions
+ *
+ * This software is provided 'as-is', without any express or implied warranty. In
+ * no event will the authors be held liable for any damages arising from the use
+ * of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including
+ * commercial applications, and to alter it and redistribute it freely, subject to
+ * the following restrictions:
+ *
+ * 1) The origin of this software must not be misrepresented; you must not claim
+ *    that you wrote the original software. If you use this software in a product,
+ *    an acknowledgment in the product documentation is appreciated.
+ *
+ * 2) Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3) This notice may not be removed or altered from any source distribution.
+ *
+ * @author antti.peuhkurinen@woimasolutions.com
+ */
+
+#ifndef LIBW_RESOURCE
+#define LIBW_RESOURCE
+
+#include "Referenced.hpp"
+#include <string>
+
+namespace w
+{
+    class Resource: public Referenced
+    {
+    public:
+        Resource(const std::string& filename);
+        virtual ~Resource();
+        const std::string& path() const;
+
+    private:
+        std::string filename_;
+    };
+}
+
+#endif
