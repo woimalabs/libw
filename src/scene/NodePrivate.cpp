938c1a2dec3ac89d143ab72aac4626bb9d8f604c
Antti Peuhkurinen <antti.peuhkurinen@woimasolutions.com>
2013-11-03 13:17:13 +0200

    ios project

--- src/scene/NodePrivate.cpp
+++ src/scene/NodePrivate.cpp
@@ -108,7 +108,9 @@ namespace w
 
             if (i == components_.end())
             {
-                components_.insert(std::make_pair<std::string, ReferencedPointer<ComponentPrivate> >(key, component.private_));
+                std::pair<std::string, ReferencedPointer<ComponentPrivate> > tmp = std::make_pair(key, component.private_);
+                components_.insert(tmp);
+                //components_.insert(std::make_pair<std::string, ReferencedPointer<ComponentPrivate> >(key, component.private_));
             }
             else
             {
