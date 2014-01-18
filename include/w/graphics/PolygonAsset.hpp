e90103f5866379f6a4f54b4a43e0e77ac0357399
Antti Peuhkurinen <antti.peuhkurinen@woimasolutions.com>
2014-01-18 19:22:09 +0200

    PolygonAsset

--- include/w/graphics/PolygonAsset.hpp
+++ include/w/graphics/PolygonAsset.hpp
@@ -40,6 +40,13 @@ namespace w
 
             struct Point
             {
+                Point(float x, float y, float z):
+                    x(x),
+                    y(y),
+                    z(z)
+                {
+                }
+
                 float x, y, z;
             };
 
